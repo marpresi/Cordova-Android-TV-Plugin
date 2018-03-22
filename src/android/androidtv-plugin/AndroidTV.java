1d0f70e914b0cd022eb9ad1f4f07a7b8313b8478
Alexey Pobery <poberyaa@gmail.com>
2018-03-22 12:18:57 +0300

    Add source file

---
+++ src/android/androidtv-plugin/AndroidTV.java
@@ -0,0 +1,27 @@
+package org.apache.cordova.plugin;
+
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CallbackContext;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.content.ActivityNotFoundException;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+
+/**
+ * * This class provides methods for TTK STB.
+ * */
+public class AndroidTV extends CordovaPlugin {
+
+	@Override
+	public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+		//TODO: impl
+		return false;
+	}
+
+}
